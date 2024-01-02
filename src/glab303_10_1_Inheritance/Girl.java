ddd2cd63b7c1a412147b6484bd31b36e9c568476
garciaedy <edygarcia97@gmail.com>
2024-01-02 16:23:09 -0500

    Push

---
+++ src/glab303_10_1_Inheritance/Girl.java
@@ -0,0 +1,18 @@
+package glab303_10_1_Inheritance;
+
+public class Girl extends Person {
+    static double   ageFactor = 1.3;
+    public Girl(String aName) {
+        name = "Ms." + aName;
+    }
+
+    public String talk() {
+        return("Hello! " + jump());
+    }
+    public String jump() {
+        return("I am jumping.");
+    }
+    public static double lifeSpan() {
+        return(lifeSpan * ageFactor);
+    }
+}
---
+++ src/glab303_3_1/JavaStringMethods.java
@@ -0,0 +1,9 @@
+package glab303_3_1;
+
+public class JavaStringMethods {
+    public static void main(String args[]){
+
+        System.out.println("Hello World");
+
+    }
+}
